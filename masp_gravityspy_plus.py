12630bfe8c12ac4716048011b3c68a71ead9fc8b
Horange <horange1024@outlook.com>
2022-04-13 11:45:59 -0500

    more auxilary channels

--- masp_gravityspy_plus.py
+++ masp_gravityspy_plus.py
@@ -2,8 +2,16 @@ from gravityspy_ligo.subject.subject import GravitySpySubject
 from gravityspy_ligo.utils import utils   
 config = utils.GravitySpyConfigFile(plot_time_ranges=[8.0, 4.0, 1.0])  
 sub = GravitySpySubject(event_time=1253940741.813, ifo="H1", manual_list_of_auxiliary_channel_names=["H1:ASC-AS_A_RF45_Q_YAW_OUT_DQ", "H1:LSC-REFL_A_LF_OUT_DQ"])
+<<<<<<< Updated upstream
+=======
+#sub = GravitySpySubject(event_time=1253940741.813, ifo="H1", manual_list_of_auxiliary_channel_names=["H1:ASC-AS_A_RF45_Q_YAW_OUT_DQ", "H1:LSC-REFL_A_LF_OUT_DQ", "H1:ASC-AS_A_RF45_Q_PIT_OUT_DQ", "H1:SUS-PR3_M3_OPLEV_PIT_OUT_DQ", "H1:LSC-REFL_A_RF45_I_ERR_DQ", "H1:LSC-POP_A_LF_OUT_DQ"])
+>>>>>>> Stashed changes
 
 sub.make_omega_scans(verbose=True, config=config)
 #sub.combine_images_for_subject_upload()
 sub.upload_to_zooniverse(subject_set_id=103434)
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
