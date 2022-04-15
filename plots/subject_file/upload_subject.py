6b78c2cc59e7cc16b85c05823668b553d55818c2
Horange <horange1024@outlook.com>
2022-04-15 15:31:25 -0500

    wrap up

--- plots/subject_file/upload_subject.py
+++ plots/subject_file/upload_subject.py
@@ -4,7 +4,7 @@ import datetime
 subject = panoptes_client.Subject()
 subject.links.project = '9979'
 subject.metadata['date'] = datetime.datetime.now().strftime('%Y%m%d')
-subject.add_location('subject_file\subject_file3x4.png')
+subject.add_location('subject_file3x4.png')
 subject.metadata['Filename1'] = 'subject_file3x4.png'
 subject.save()
 subjectset = panoptes_client.SubjectSet.find(103434)
