30fe1cc9b567606567c4da1ba67ae8bd15bda9aa
Horange <horange1024@outlook.com>
2022-04-15 11:57:39 -0500

    upload_subject.py

---
+++ plots/upload_subject.py
@@ -0,0 +1,15 @@
+import panoptes_client
+import datetime
+
+subject = panoptes_client.Subject()
+subject.links.project = '9979'
+subject.metadata['date'] = datetime.datetime.now().strftime('%Y%m%d')
+subject.add_location('subject_file\subject_file3x4.png')
+subject.metadata['Filename1'] = 'subject_file3x4.png'
+subject.save()
+subjectset = panoptes_client.SubjectSet.find(103434)
+subjectset.add(subject)
+
+
+
+# fatal error - Internal error: TP_NUM_C_BUFS too small: 50
