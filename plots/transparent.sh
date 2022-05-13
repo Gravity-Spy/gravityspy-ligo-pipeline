805590d05e3abb8692f4f295711abaf446ae3890
Renzhi Hao <renzhi.hao@ldas-pcdev6.ligo-wa.caltech.edu>
2022-05-12 21:17:01 -0700

    no plots

--- plots/transparent.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-magick convert H1_D8LC2PvaimingzzYvEPo_H1-GDS-CALIB_STRAIN_spectrogram_1.0.png -crop 575x480+100+60 -alpha set -channel A -evaluate multiply 0.4 -channel G -evaluate multiply 0.2 -channel B -evaluate multiply 0.2 half_transparent.png
-magick composite half_transparent.png  H1_D8LC2PvaimingzzYvEPo_H1-ASC-AS_A_RF45_Q_PIT_OUT_DQ_spectrogram_1.0.png -gravity center -geometry -13 -background none modified.png
-rm half_transparent.png
